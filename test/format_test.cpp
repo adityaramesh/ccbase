9258df40b7af753d2fbae8d7299d3f4bef481fd8
Aditya Ramesh <_@adityaramesh.com>
2013-07-02 01:36:41 -0700

    Revised unit tests.

--- test/format_test.cpp
+++ test/format_test.cpp
@@ -5,12 +5,20 @@
 ** Contact:	_@adityaramesh.com
 */
 
-#include <cstddef>
 #include <ccbase/format.hpp>
+#include <ccbase/unit_test.hpp>
 
-int main()
-{
-	cc::println("These are three arguments: {0}, {1}, and {0}.", "Hello", 0.995);
-	cc::println("Escaping curly backets: {{, }}, {{{{, }}}}.");
-	return EXIT_SUCCESS;
-}
+CC_BEGIN_MODULE(test_repeated_arguments)
+	auto s = cc::format("{0} {1} {0}", "A", 1);
+	CC_ASSERT(s == "A 1 A");
+CC_END_MODULE(test_repeated_arguments)
+
+CC_BEGIN_MODULE(test_escape)
+	auto s = cc::format("{{}}{{{{}}}}");
+	CC_ASSERT(s == "{}{{}}");
+CC_END_MODULE(test_escape)
+
+CC_BEGIN_SUITE(format_test)
+	test_repeated_arguments();
+	test_escape();
+CC_END_SUITE(format_test)
