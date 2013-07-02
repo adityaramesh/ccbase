a5100f01d76e5b7ee60046f5afc320e92053feb2
Aditya Ramesh <_@adityaramesh.com>
2013-07-02 01:02:18 -0700

    Added support for references to expected.

--- ccbase/expected.hpp
+++ ccbase/expected.hpp
@@ -9,17 +9,23 @@
 ** This class is based on Andrei Alexandrescu's implementation of `expected<T>`,
 ** which he discussed in a talk at C++ Next 2012 called "Systematic Error
 ** Handling". The video and slides of the talk (which contain his code) are
-** available
-** [here](http://channel9.msdn.com/Shows/Going+Deep/C-and-Beyond-2012-Andrei-Alexandrescu-Systematic-Error-Handling-in-C).
+** available [here][error_handling]. Support was later added for
+** `expected<void>`, so that methods that return void can still take advantage
+** of the this idiom. Finally, support for references was added, so that objects
+** of type `expected<T&>` and `expected<const T&>` can now be created. The fact
+** that generalized unions cannot contain references was alluded to in [this
+** blog post][extending_expected], and the approach taken to rectify the problem
+** is adapted from the one given in the blog post.
 **
 ** # Introduction
 ** 
 ** From the horse's mouth: `expected<T>` is a variant type that either contains
-** `T` or the exception that prevented its creation. It allows you to perform
-** some action that may raise an exception, and postpone dealing with the
-** exception until it is convenient. This approach essentially gives you the
-** ability to return "arbitrarily rich error codes." See Alexandrescu's talk for
-** a great discussion of its uses, as well as a walkthrough of the design.
+** `T` or the exception that prevented its creation. It allows you to perform an
+** action that may raise an exception, and postpone dealing with the exception
+** until it is convenient. This approach essentially gives you the ability to
+** return "arbitrarily rich error codes." See [Alexandrescu's
+** talk][error_handling] for a great discussion of its uses, as well as a
+** walkthrough of the design.
 **
 ** This implementation of `expected<T>` is based on Alexandrescu's, but it has
 ** some modifications, namely:
@@ -35,18 +41,24 @@
 **
 ** 2. I made Alexandrescu's `fromCode` function global, and used `decltype` to
 ** infer the return type of functor or lambda. The name of the function has been
-** changed to `attempt`. The return type has been wrapped in an
-** `std::enable_if`, so that if the user-provided functor returns void (as a
-** result of a programming error), the compiler barfs less.
+** changed to `attempt`. Methods that return values, pointers, references, and
+** even void are all supported.
 **
 ** 3. There is a specialization for `expected<void>`, with the obvious
 ** modifications.
+**
+** 4. There are specializations for `expected<T&>` and `expected<const T&>`, as
+** described by [this blog post][extending_expected].
+**
+** [error_handling]: http://channel9.msdn.com/Shows/Going+Deep/C-and-Beyond-2012-Andrei-Alexandrescu-Systematic-Error-Handling-in-C "Alexandrescu's Talk on Systematic Error Handling"
+** [extending_expected] : http://anto-nonco.blogspot.com/2013/03/extending-expected-to-deal-with.html "Extending `expected<T>` to Deal with References"
 */
 
 #ifndef Z9F71ED6B_DBA6_4FFD_83F7_2D168D140002
 #define Z9F71ED6B_DBA6_4FFD_83F7_2D168D140002
 
 #include <exception>
+#include <functional>
 #include <stdexcept>
 #include <type_traits>
 #include <typeinfo>
@@ -56,46 +68,82 @@ namespace cc
 {
 
 template <class T>
-class expected
+struct expected_traits
 {
+	using storage   = T;
+	using value     = T;
+	using pointer   = T*;
+	using reference = T&;
+};
+
+template <class T>
+struct expected_traits<T&>
+{
+	using storage   = std::reference_wrapper<T>;
+	using value     = T;
+	using pointer   = T*;
+	using reference = T&;
+};
+
+template <class T>
+struct expected_traits<const T&>
+{
+	using storage   = std::reference_wrapper<const T>;
+	using value     = T;
+	using pointer   = const T*;
+	using reference = const T&;
+};
+
+template <class> class expected;
+
+template <class T>
+class expected_base
+{
+	friend class expected<T>;
+
+	using storage   = typename expected_traits<T>::storage;
+	using value     = typename expected_traits<T>::value;
+	using pointer   = typename expected_traits<T>::pointer;
+	using reference = typename expected_traits<T>::reference;
+
 	union
 	{
-		T t_;
+		storage t_;
 		std::exception_ptr p_;
 	};
 
 	bool valid_;
-	
-public:
+
 	/*
 	** These constructors are used to create `expected` objects in valid
 	** states.
 	*/
 
-	expected(const T& rhs) : t_{rhs}, valid_{true} {}
-	expected(T&& rhs) : t_{std::move(rhs)}, valid_{true} {}
-	
+	expected_base(value& rhs) noexcept : t_{rhs}, valid_{true} {}
+	expected_base(const value& rhs) noexcept : t_{rhs}, valid_{true} {}
+	expected_base(value&& rhs) noexcept : t_{std::move(rhs)}, valid_{true} {}
+
 	/*
 	** These constructors are used to create `expected` objects in invalid
 	** states.
 	*/
 
-	expected() : valid_{false}
+	expected_base() noexcept : valid_{false}
 	{
 		new (&p_) std::exception_ptr{std::move(std::current_exception())};
 	}
 	
-	expected(std::exception_ptr p) : valid_{false}
+	expected_base(std::exception_ptr p) noexcept : valid_{false}
 	{
 		new (&p_) std::exception_ptr{std::move(p)};
 	}
 
 	template <class E>
-	expected(const E& e,
+	expected_base(const E& e,
 		typename std::enable_if<
 			std::is_base_of<std::exception, E>::value, E
 		>::type* = 0
-	) : valid_{false}
+	) noexcept : valid_{false}
 	{
 		if (typeid(e) != typeid(E)) {
 			throw std::invalid_argument{"Slicing detected."};
@@ -103,46 +151,47 @@ public:
 		new (&p_) std::exception_ptr{std::move(std::make_exception_ptr(e))};
 	}
 
-	expected(const expected& rhs) : valid_{rhs.valid_}
+	expected_base(const expected_base& rhs) noexcept : valid_{rhs.valid_}
 	{
-		if (valid_) new(&t_) T{rhs.t_};
+		if (valid_) new(&t_) storage{rhs.t_};
 		else new(&p_) std::exception_ptr{rhs.p_};
 	}
 
-	expected(expected&& rhs) : valid_{rhs.valid_}
+	expected_base(expected_base&& rhs) noexcept : valid_{rhs.valid_}
 	{
-		if (valid_) new(&t_) T{std::move(rhs.t_)};
+		if (valid_) new(&t_) storage{std::move(rhs.t_)};
 		else new(&p_) std::exception_ptr{std::move(rhs.p_)};
 	}
 
-	~expected()
+	~expected_base()
 	{
 		// We need the `using` statement here, because `::~` does not
 		// parse, so we would not be able to not write
 		// `p_.::~exception_ptr()`.
 		using std::exception_ptr;
-		if (valid_) t_.~T();
+		if (valid_) t_.~storage();
 		else p_.~exception_ptr();
 	}
 
+public:
 	bool valid() const
 	{
 		return valid_;
 	}
 
-	T& get()
+	reference get()
 	{
 		if (!valid_) std::rethrow_exception(p_);
 		return t_;
 	}
 
-	const T& get() const
+	const reference get() const
 	{
 		if (!valid_) std::rethrow_exception(p_);
 		return t_;
 	}
 
-	void swap(expected& rhs)
+	void swap(expected_base& rhs)
 	{
 		if (valid_) {
 			if (rhs.valid_) {
@@ -151,22 +200,22 @@ public:
 			}
 			else {
 				// We need to either move `rhs`'s
-				// `std::exception_ptr` or our `T` into a
-				// temporary. When we mutate a member of the
-				// union of either the LHS or the RHS, we will
-				// invalidate the other member of that union. We
-				// make the assumption that choosing to load
-				// whichever of the two is smaller (either `T`
-				// or `std::exception_ptr`) into a temporary
-				// will be most efficient.
-				if (sizeof(T) < sizeof(std::exception_ptr)) {
+				// `std::exception_ptr` or our `storage` object
+				// into a temporary. When we mutate a member of
+				// the union of either the LHS or the RHS, we
+				// will invalidate the other member of that
+				// union. We make the assumption that choosing
+				// to load whichever of the two is smaller
+				// (either `storage` or `std::exception_ptr`)
+				// into a temporary will be most efficient.
+				if (sizeof(storage) < sizeof(std::exception_ptr)) {
 					auto t = std::move(t_);
 					new (&p_) std::exception_ptr{rhs.p_};
-					new (&rhs.t_) T{std::move(t)};
+					new (&rhs.t_) storage{std::move(t)};
 				}
 				else {
 					auto t = std::move(rhs.p_);
-					new (&rhs.t_) T{std::move(t_)};
+					new (&rhs.t_) storage{std::move(t_)};
 					new (&p_) std::exception_ptr{t};
 				}
 				valid_ = false;
@@ -175,14 +224,14 @@ public:
 		}
 		else {
 			if (rhs.valid_) {
-				if (sizeof(T) < sizeof(std::exception_ptr)) {
+				if (sizeof(storage) < sizeof(std::exception_ptr)) {
 					auto t = std::move(rhs.t_);
 					new (&rhs.p_) std::exception_ptr{p_};
-					new (&t_) T{std::move(t)};
+					new (&t_) storage{std::move(t)};
 				}
 				else {
 					auto t = std::move(p_);
-					new (&t_) T{std::move(rhs.t_)};
+					new (&t_) storage{std::move(rhs.t_)};
 					new (&rhs.p_) std::exception_ptr{t};
 				}
 				rhs.valid_ = false;
@@ -211,6 +260,94 @@ public:
 	}
 };
 
+template <class T>
+class expected : public expected_base<T>
+{
+private:
+	friend class expected_base<T>;
+	using base = expected_base<T>;
+public:
+	expected(const T& rhs) noexcept : base{rhs} {}
+	expected(T&& rhs) noexcept : base{std::move(rhs)} {}
+
+	expected() noexcept : base{} {}
+	expected(std::exception_ptr p) noexcept : base{p} {}
+
+	template <class E>
+	expected(const E& e,
+		typename std::enable_if<
+			std::is_base_of<std::exception, E>::value, E
+		>::type* = 0
+	) noexcept : base{e} {}
+
+	expected(const expected& rhs) noexcept :
+	base{static_cast<const base&>(rhs)} {}
+
+	expected(expected&& rhs) noexcept :
+	base{static_cast<base&&>(rhs)} {}
+
+	void swap(expected& rhs)
+	{
+		base::swap(static_cast<base&>(rhs));
+	}
+};
+
+template <class T>
+class expected<T&> : public expected_base<T&>
+{
+private:
+	friend class expected_base<T&>;
+	using base = expected_base<T&>;
+public:
+	expected(T& rhs) : base{rhs} {}
+
+	expected() : base{} {}
+	expected(std::exception_ptr p) : base{p} {}
+
+	template <class E>
+	expected(const E& e,
+		typename std::enable_if<
+			std::is_base_of<std::exception, E>::value, E
+		>::type* = 0
+	) : base{e} {}
+
+	expected(const expected& rhs) : base{static_cast<const base&>(rhs)} {}
+	expected(expected&& rhs) : base{static_cast<base&&>(rhs)} {}
+
+	void swap(expected& rhs)
+	{
+		base::swap(static_cast<base&>(rhs));
+	}
+};
+
+template <class T>
+class expected<const T&> : public expected_base<const T&>
+{
+private:
+	friend class expected_base<const T&>;
+	using base = expected_base<const T&>;
+public:
+	expected(const T& rhs) : base{rhs} {}
+
+	expected() : base{} {}
+	expected(std::exception_ptr p) : base{p} {}
+
+	template <class E>
+	expected(const E& e,
+		typename std::enable_if<
+			std::is_base_of<std::exception, E>::value, E
+		>::type* = 0
+	) : base{e} {}
+
+	expected(const expected& rhs) : base{static_cast<const base&>(rhs)} {}
+	expected(expected&& rhs) : base{static_cast<base&&>(rhs)} {}
+
+	void swap(expected& rhs)
+	{
+		base::swap(static_cast<base&>(rhs));
+	}
+};
+
 /*
 ** You should use this specialization when you want to return an expected object
 ** from a method that would otherwise return `void`. This specialization does
@@ -227,19 +364,19 @@ class expected<void>
 	std::exception_ptr p_;
 	bool valid_;
 public:
-	expected(bool) : valid_{true} {}
+	expected(bool) noexcept : valid_{true} {}
 
 	/*
 	** These constructors are used to create `expected` objects in invalid
 	** states.
 	*/
 
-	expected() : valid_{false}
+	expected() noexcept : valid_{false}
 	{
 		new (&p_) std::exception_ptr{std::move(std::current_exception())};
 	}
 	
-	expected(std::exception_ptr p) : valid_{false}
+	expected(std::exception_ptr p) noexcept : valid_{false}
 	{
 		new (&p_) std::exception_ptr{std::move(p)};
 	}
@@ -249,7 +386,7 @@ public:
 		typename std::enable_if<
 			std::is_base_of<std::exception, E>::value, E
 		>::type* = 0
-	) : valid_{false}
+	) noexcept : valid_{false}
 	{
 		if (typeid(e) != typeid(E)) {
 			throw std::invalid_argument{"Slicing detected."};
@@ -257,12 +394,12 @@ public:
 		new (&p_) std::exception_ptr{std::move(std::make_exception_ptr(e))};
 	}
 
-	expected(const expected& rhs) : valid_{rhs.valid_}
+	expected(const expected& rhs) noexcept : valid_{rhs.valid_}
 	{
 		if (!valid_) new(&p_) std::exception_ptr{rhs.p_};
 	}
 
-	expected(expected&& rhs) : valid_{rhs.valid_}
+	expected(expected&& rhs) noexcept : valid_{rhs.valid_}
 	{
 		if (!valid_) new(&p_) std::exception_ptr{std::move(rhs.p_)};
 	}
@@ -327,8 +464,9 @@ public:
 };
 
 /*
-** We wrap the return type in an `std::enable_if`, so that the compiler does not
-** barf (as much) when the callable object `f` returns `void`.
+** We need to use SFINAE, because `expected<void>` objects have different
+** construction semantics than the other specializations. This is because we
+** cannot pass `void` as an argument.
 */
 
 template <class F>
@@ -344,6 +482,22 @@ typename std::enable_if<
 	catch (...) {
 		return {};
 	}
+}
+
+template <class F>
+auto attempt(F f) ->
+typename std::enable_if<
+	std::is_same<decltype(f()), void>::value,
+	expected<void>
+>::type
+{
+	try {
+		f();
+	}
+	catch (...) {
+		return {};
+	}
+	return { true };
 }
 
 }
--- test/expected_test.cpp
+++ test/expected_test.cpp
@@ -5,26 +5,96 @@
 ** Contact:	_@adityaramesh.com
 */
 
-#include <cstddef>
-#include <stdexcept>
-#include <iostream>
+#include <exception>
 #include <ccbase/expected.hpp>
+#include <ccbase/unit_test.hpp>
 
-static int foo()
-{
-	throw std::invalid_argument{"poo"};
-}
+CC_BEGIN_MODULE(test_valid_constructors)
+	int a{1};
+	cc::expected<int> e1{a};
+	cc::expected<int> e2{std::move(a)};
+	CC_ASSERT(e1.valid());
+	CC_ASSERT(e2.valid());
 
-static cc::expected<int> bar(int a)
-{
-	if (a) return 1;
-	return std::invalid_argument{"bar"};
-}
+	int b{1};
+	cc::expected<int&> e3{b};
+	CC_ASSERT(e3.valid());
+	CC_ASSERT(e3.get() == 1);
 
-int main()
-{
-	//auto r = cc::attempt([] { return foo(); });
-	//r.get();
-	bar(0).get();
-	return EXIT_SUCCESS;
-}
+	e3.get() = 2;
+	CC_ASSERT(e3.valid());
+	CC_ASSERT(e3.get() == 2);
+CC_END_MODULE(test_valid_constructors)
+
+CC_BEGIN_MODULE(test_invalid_constructors)
+	std::exception e;
+	std::exception_ptr ep{std::move(std::make_exception_ptr(e))};
+
+	cc::expected<int> e1{};
+	cc::expected<int> e2{e};
+	cc::expected<int> e3{ep};
+
+	CC_ASSERT(!e1.valid());
+	CC_ASSERT(!e2.valid());
+	CC_ASSERT(!e3.valid());
+CC_END_MODULE(test_invalid_constructors)
+
+CC_BEGIN_MODULE(test_swap)
+	int a{1};
+	int b{2};
+	cc::expected<int> e1{a};
+	cc::expected<int> e2{b};
+	CC_ASSERT(e1.valid());
+	CC_ASSERT(e2.valid());
+	CC_ASSERT(e1.get() == 1);
+	CC_ASSERT(e2.get() == 2);
+
+	e1.swap(e2);
+	CC_ASSERT(e1.valid());
+	CC_ASSERT(e2.valid());
+	CC_ASSERT(e1.get() == 2);
+	CC_ASSERT(e2.get() == 1);
+
+	cc::expected<int> e3{std::exception{}};
+	cc::expected<int> e4{a};
+	CC_ASSERT(!e3.valid());
+	CC_ASSERT(e4.valid());
+	CC_ASSERT(e4.get() == 1);
+
+	e3.swap(e4);
+	CC_ASSERT(e3.valid());
+	CC_ASSERT(!e4.valid());
+	CC_ASSERT(e3.get() == 1);
+
+	int c{1};
+	int d{2};
+	cc::expected<int&> e5{c};
+	cc::expected<int&> e6{d};
+	CC_ASSERT(e5.valid());
+	CC_ASSERT(e6.valid());
+	CC_ASSERT(e5.get() == 1);
+	CC_ASSERT(e6.get() == 2);
+
+	e5.swap(e6);
+	CC_ASSERT(e5.valid());
+	CC_ASSERT(e6.valid());
+	CC_ASSERT(e5.get() == 2);
+	CC_ASSERT(e6.get() == 1);
+
+	cc::expected<int&> e7{std::exception{}};
+	cc::expected<int&> e8{c};
+	CC_ASSERT(!e7.valid());
+	CC_ASSERT(e8.valid());
+	CC_ASSERT(e8.get() == 1);
+
+	e7.swap(e8);
+	CC_ASSERT(!e8.valid());
+	CC_ASSERT(e7.valid());
+	CC_ASSERT(e7.get() == 1);
+CC_END_MODULE(test_get)
+
+CC_BEGIN_SUITE(expected_test)
+	test_valid_constructors();
+	test_invalid_constructors();
+	test_swap();
+CC_END_SUITE(expected_test)
