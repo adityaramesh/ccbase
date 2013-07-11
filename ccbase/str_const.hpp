31771cc472ac88dc078d28b5a69cb4bdcbf5bd92
Aditya Ramesh <_@adityaramesh.com>
2013-07-10 18:19:28 -0700

    Fixed const-correctness.

--- ccbase/str_const.hpp
+++ ccbase/str_const.hpp
@@ -33,22 +33,22 @@ public:
 	constexpr str_const(const char(&a)[N]) :
 	p_{a}, s_{N - 1} {}
 
-	constexpr char operator[](std::size_t n)
+	constexpr char operator[](std::size_t n) const
 	{
 		return n < s_ ? p_[n] : throw std::out_of_range{""};
 	}
 
-	constexpr std::size_t length()
+	constexpr std::size_t length() const
 	{
 		return s_;
 	}
 
-	constexpr std::size_t size()
+	constexpr std::size_t size() const
 	{
 		return s_;
 	}
 
-	constexpr operator const char*()
+	constexpr operator const char*() const
 	{
 		return p_;
 	}
