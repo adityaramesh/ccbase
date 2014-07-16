eca7dd14fab0ee388e42b07fae788fe23b4f9489
Aditya Ramesh <_@adityaramesh.com>
2014-07-15 17:47:18 -0700

    Added setter that uses move assignment.

--- include/ccbase/utility/accessors.hpp
+++ include/ccbase/utility/accessors.hpp
@@ -8,6 +8,8 @@
 #ifndef Z19D16F4C_D8F1_4708_ABE5_EBFBF560B64D
 #define Z19D16F4C_D8F1_4708_ABE5_EBFBF560B64D
 
+#include <utility>
+
 #define DEFINE_REF_GETTER(name, member)                         \
 	decltype(member)& name() { return member; }             \
 	const decltype(member)& name() const { return member; }
@@ -19,6 +21,12 @@
 	self& name(const decltype(member)& x) \
 	{                                     \
 		this->member = x;             \
+		return *this;                 \
+	}                                     \
+                                              \
+	self& name(decltype(member)&& x)      \
+	{                                     \
+		this->member = std::move(x);  \
 		return *this;                 \
 	}
 
