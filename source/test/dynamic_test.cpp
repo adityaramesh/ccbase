87011a30251f785479c353ce1b8efd80ad4d3ec6
Aditya Ramesh <_@adityaramesh.com>
2014-08-15 15:36:20 -0400

    All unit tests now passing.

--- source/test/dynamic_test.cpp
+++ source/test/dynamic_test.cpp
@@ -15,39 +15,39 @@ module("test_retrieval", "Tests whether loading data works.")
 {
 	using signature = int(const std::string&, const std::string&);
 	auto img = cc::image{"./out/libraries/test.lib", cc::binding_mode::lazy};
-	auto f = cc::load<signature>("test", img).get();
-	auto& s = cc::load<std::string>("msg", img).get();
 
+	auto f = *cc::load<signature>("test", img);
+	auto& s = *cc::load<std::string>("msg", img);
 	require(f("1", "00") == 100);
 	require(s == "Original contents.");
 }
 
-/*
 module("test_mutation", "Tests whether module data is mutable.")
 {
 	auto img = cc::image{"./out/libraries/test.lib", cc::binding_mode::lazy};
-	auto& s1 = cc::load<std::string>("msg", img).get();
+
+	auto& s1 = *cc::load<std::string>("msg", img);
 	require(s1 == "Original contents.");
 	s1 = "New contents.";
 
-	auto& s2 = cc::load<std::string>("msg", img).get();
+	auto& s2 = *cc::load<std::string>("msg", img);
 	require(s2 == "New contents.");
 }
 
 module("test_info", "Tests whether symbol address lookup works.")
 {
-	using signature = std::string(std::string, std::string);
+	using signature = int(const std::string&, const std::string&);
 	auto img = cc::image{"./out/libraries/test.lib", cc::binding_mode::lazy};
-	auto f = cc::load<signature>("test", img).get();
-	require(f("foo", "bar") == "foobar");
-	auto& s = cc::load<std::string>("msg", img).get();
+
+	auto f = *cc::load<signature>("test", img);
+	auto& s = *cc::load<std::string>("msg", img);
+	require(f("1", "00") == 100);
 	require(s == "Original contents.");
 
-	auto fi = cc::query(f).get();
-	auto si = cc::query(s).get();
+	auto fi = *cc::query(f);
+	auto si = *cc::query(s);
 	require(fi.name() == "test");
 	require(si.name() == "msg");
 }
-*/
 
 suite("Tests dynamic library loading.")
